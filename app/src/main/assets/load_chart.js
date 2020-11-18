6e4e6d617a66ded3deccd10c4b60729711274bc7
jifchen07 <jifanche@usc.edu>
2020-11-18 00:02:54 -0800

    added stockchart in the scrollview

---
+++ app/src/main/assets/load_chart.js
@@ -0,0 +1,168 @@
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == variable) {
+      return pair[1];
+    }
+  }
+}
+
+function timestampToYYYYMMDD(timestamp) {
+  let month = '' + (timestamp.getMonth() + 1);
+  let day = '' + timestamp.getDate();
+  let year = timestamp.getFullYear();
+
+  if (month.length < 2) {
+    month = '0' + month;
+  }
+  if (day.length < 2) {
+    day = '0' + day;
+  }
+
+  return [year, month, day].join('-');
+}
+
+const ticker = getQueryVariable("ticker");;
+
+const date = new Date();
+date.setFullYear(date.getFullYear() - 2);
+const fromDate = timestampToYYYYMMDD(date);
+console.log(fromDate);
+
+/* const url = "https://stock-search-backend-110320.wl.r.appspot.com/search/prices/amzn?startDate="
++ fromDate
++ "&resampleFreq=12hour" */
+
+const url = `https://stock-search-backend-110320.wl.r.appspot.com/search/prices/${ticker}?startDate=${fromDate}&resampleFreq=12hour`
+
+console.log(url);
+
+
+Highcharts.getJSON(url, function (data) {
+
+    // split the data set into ohlc and volume
+    var ohlc = [],
+        volume = [],
+        dataLength = data.length,
+        // set the allowed units for data grouping
+        groupingUnits = [[
+            'week',                         // unit name
+            [1]                             // allowed multiples
+        ], [
+            'month',
+            [1, 2, 3, 4, 6]
+        ]],
+
+        i = 0;
+
+    for (i; i < dataLength; i += 1) {
+    		let date = new Date(data[i].date.substring(0, 10));
+        let timestamp = Date.parse(date.toString());
+        ohlc.push([
+        		timestamp,
+            data[i].open,
+            data[i].high,
+            data[i].low,
+            data[i].close
+        ]);
+
+        volume.push([
+            timestamp,
+            data[i].volume // the volume
+        ]);
+    }
+
+
+    // create the chart
+    Highcharts.stockChart('container', {
+
+        rangeSelector: {
+            selected: 2
+        },
+
+        title: {
+            text: `${ticker} Historical`
+        },
+
+        subtitle: {
+            text: 'With SMA and Volume by Price technical indicators'
+        },
+
+        yAxis: [{
+            startOnTick: false,
+            endOnTick: false,
+            labels: {
+                align: 'right',
+                x: -3
+            },
+            title: {
+                text: 'OHLC'
+            },
+            height: '60%',
+            lineWidth: 2,
+            resize: {
+                enabled: true
+            }
+        }, {
+            labels: {
+                align: 'right',
+                x: -3
+            },
+            title: {
+                text: 'Volume'
+            },
+            top: '65%',
+            height: '35%',
+            offset: 0,
+            lineWidth: 2
+        }],
+
+        tooltip: {
+        		valueDecimals: 2,
+            split: true
+        },
+
+        plotOptions: {
+            series: {
+                dataGrouping: {
+                   units: groupingUnits
+                }
+            }
+        },
+
+        series: [{
+            type: 'candlestick',
+            name: `${ticker}`,
+            id: 'upper',
+            zIndex: 2,
+            data: ohlc
+        }, {
+            type: 'column',
+            name: 'Volume',
+            id: 'volume',
+            data: volume,
+            yAxis: 1
+        }, {
+            type: 'vbp',
+            linkedTo: 'upper',
+            params: {
+                volumeSeriesID: 'volume'
+            },
+            dataLabels: {
+                enabled: false
+            },
+            zoneLines: {
+                enabled: false
+            }
+        }, {
+            type: 'sma',
+            linkedTo: 'upper',
+            zIndex: 1,
+            marker: {
+                enabled: false
+            }
+        }]
+    });
+});
--- app/src/main/java/com/example/stockapp/DetailActivity.java
+++ app/src/main/java/com/example/stockapp/DetailActivity.java
@@ -3,14 +3,48 @@ package com.example.stockapp;
 import android.content.Intent;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class DetailActivity extends AppCompatActivity {
     private static final String TAG = "DetailActivity";
     private Stock stock;
     private MyApplication appData;
+
+    private String ticker;
+    private String name;
+    private String description;
+
+    private Double lastPrice;
+    private Double changePrice;
+    private Double lowPrice;
+    private Double midPrice;
+    private Double highPrice;
+    private Double bidPrice;
+    private Double openPrice;
+
+    private int volume;
+
+    private WebView webView;
+
+
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,15 +69,81 @@ public class DetailActivity extends AppCompatActivity {
         Intent intent = getIntent();
 
         stock = (Stock) intent.getSerializableExtra(MainActivity.EXTRA_TICKER);
+        ticker = stock.getTicker();
 
         appData = (MyApplication) getApplicationContext();
 
         Log.d(TAG, "onCreate: " + stock.getTicker());
         Log.d(TAG, "onCreate: " + appData.getPortfolioList());
         Log.d(TAG, "onCreate: " + appData.getWatchList());
+
+        queue = Volley.newRequestQueue(getApplicationContext());
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled (true);
+        webView.loadUrl("file:///android_asset/highchart.html?ticker=" + ticker);
     }
 
-    private void fetchData() {
+    private void fetchDescription() {
+        String url = "https://stock-search-backend-110320.wl.r.appspot.com/search/description/" + ticker;
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
 
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            name = response.getString("name");
+                            description = response.getString("description");
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+        queue.add(jsonObjectRequest);
     }
+
+    private void fetchLatestPrice() {
+        String url = "https://stock-search-backend-110320.wl.r.appspot.com/search/latestprice/" + ticker;
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            JSONObject data = response.getJSONObject(0);
+                            lastPrice = data.getDouble("last");
+                            changePrice = lastPrice - data.getDouble("prevClose");
+                            lowPrice = data.getDouble("low");
+                            midPrice = data.getDouble("mid");
+                            highPrice = data.getDouble("high");
+                            bidPrice = data.getDouble("bid");
+                            openPrice = data.getDouble("open");
+                            volume = data.getInt("volume");
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+        queue.add(jsonArrayRequest);
+    }
+
 }
